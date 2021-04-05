3715f0db9c539e362967e0a0e0b0460f75f850c1
nilspolek <nname6617@gmail.com>
2021-04-05 14:42:45 +0200

    version1

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hallo Welt")
+import sqlite3
+import datetime
+print(datetime.date.today())
+print(type(datetime.date.day))
+conn = sqlite3.connect('Test.db')
+
+c = conn.cursor()
+
+#c.execute("""CREATE TABLE employees (
+#        date text,
+#        infected integer
+#        )""")
+conn.commit()
+
+conn.close()
